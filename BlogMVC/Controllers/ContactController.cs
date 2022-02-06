595ca52259b335427b39244157efc4d4b9b90554
alperenmutlu <chetto_17@hotmail.com>
2022-02-06 19:33:01 +0300

    About,Contact page edited.

---
+++ BlogMVC/Controllers/ContactController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Conrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BlogMVC.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager contactManager = new ContactManager(new EfContactRepository());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact contact) 
+        {
+            contact.ContactDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            contact.ContactStatus = true;
+            contactManager.ContactAdd(contact);
+            return RedirectToAction("Index","Blog");
+        }
+    }
+}
